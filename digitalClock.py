15bb83f2a89f33fdb5d14aca5d4fc97a426c9f4f
gomezsdarwin <gomezsdarwin@gmail.com>
2022-04-04 16:12:23 -0400

    removed welcome

--- digitalClock.py
+++ digitalClock.py
@@ -1,31 +1,11 @@
-# importing whole module
-from tkinter import *
-from tkinter.ttk import *
+from tkinter import Tk, Label
+
+window = Tk()
+window.title('Digital Clock')
+window.geometry("600x300")
+window.configure(bg="red")
  
-# importing strftime function to
-# retrieve system's time
-from time import strftime
- 
-# creating tkinter window
-root = Tk()
-root.title('Clock')
- 
-# This function is used to
-# display time on the label
-def time():
-    string = strftime('%H:%M:%S %p')
-    lbl.config(text = string)
-    lbl.after(1000, time)
- 
-# Styling the label widget so that clock
-# will look more attractive
-lbl = Label(root, font = ('calibri', 40, 'bold'),
-            background = 'purple',
-            foreground = 'white')
- 
-# Placing clock at the centre
-# of the tkinter window
-lbl.pack(anchor = 'center')
-time()
- 
-mainloop()
+label = Label(window, font=("Arial Black",78,"bold"), bg="red", fg="white")
+label.pack(pady=100)
+
+window.mainloop()
